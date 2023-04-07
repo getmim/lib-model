c855232c466cf170745e15623ee9d29c0ec83e60
Iqbal Fauzi <iqbalfawz@gmail.com>
2023-04-07 22:05:58 +0700

    Apply format type on object-switch

--- modules/lib-model/formatter/Model.php
+++ modules/lib-model/formatter/Model.php
@@ -13,75 +13,86 @@ use LibFormatter\Object\Number;
 
 class Model
 {
-    private static function asArray(array $values): array{
+    private static function asArray(array $values): array
+    {
         $result = [];
-        foreach($values as $val)
+        foreach ($values as $val) {
             $result[$val] = [];
+        }
         return $result;
     }
 
-    private static function asId(array $values): array{
+    private static function asId(array $values): array
+    {
         $result = [];
-        foreach($values as $val)
+        foreach ($values as $val) {
             $result[$val] = new Std($val);
+        }
         return $result;
     }
 
-    private static function asNull(array $values): array{
+    private static function asNull(array $values): array
+    {
         $result = [];
-        foreach($values as $val)
+        foreach ($values as $val) {
             $result[$val] = null;
+        }
         return $result;
     }
 
-    private static function procValues(array $ids, object $format, $options, string $key_prop = null): array{
+    private static function procValues(array $ids, object $format, $options, string $key_prop = null): array
+    {
         $model = $format->model;
         $model_name  = $model->name;
         $model_field = $model->field ?? 'id';
-        if(!$key_prop)
+        if (!$key_prop) {
             $key_prop = $model_field;
+        }
 
         $where = [
             $model_field => $ids
         ];
-        if(is_array($options) && isset($options['_where'])){
+        if (is_array($options) && isset($options['_where'])) {
             $where = array_replace($where, $options['_where']);
             unset($options['_where']);
         }
         $rows = $model_name::get($where);
 
-        if(!$rows)
+        if (!$rows) {
             return [];
+        }
 
         $as_key = prop_as_key($rows, $key_prop);
 
         // filter one field only
-        if(isset($format->field)){
+        if (isset($format->field)) {
             $tmp_as_key = [];
-            foreach($as_key as $id => $object){
+            foreach ($as_key as $id => $object) {
                 $fname = $format->field->name;
                 $ftype = $format->field->type ?? null;
                 $used_val = $object->$fname;
 
-                if($ftype)
+                if ($ftype) {
                     $used_val = Formatter::typeApply($ftype, $used_val, $fname, $object, (object)[], null);
+                }
 
                 $tmp_as_key[$id] = $used_val;
             }
             $as_key = $tmp_as_key;
 
-        }elseif(isset($format->fields)){
+        } elseif (isset($format->fields)) {
             $tmp_as_key = [];
-            foreach($as_key as $id => $object){
+            foreach ($as_key as $id => $object) {
                 $used_vals = (object)[];
-                foreach($format->fields as $field){
+                foreach ($format->fields as $field) {
                     $fname = $field->name;
                     $ftype = $field->type ?? null;
 
                     $used_val = $object->$fname;
 
-                    if($ftype)
+                    if ($ftype) {
                         $used_val = Formatter::typeApply($ftype, $used_val, $fname, $object, (object)[], null);
+                    }
 
                     $used_vals->$fname = $used_val;
                 }
@@ -90,18 +101,21 @@ class Model
             $as_key = $tmp_as_key;
         }
 
-        if(isset($format->format) && !isset($format->field)){
-            if(!is_array($options))
+        if (isset($format->format) && !isset($format->field)) {
+            if (!is_array($options)) {
                 $options = [];
+            }
             $as_key = Formatter::formatMany($format->format, $as_key, $options, $key_prop);
         }
 
         return $as_key;
     }
 
-    static function chain(array $values, string $field, array $objects, object $format, $options): array{
-        if(is_null($options))
+    static function chain(array $values, string $field, array $objects, object $format, $options): array
+    {
+        if (is_null($options)) {
             return self::asArray($values);
+        }
 
         $chain = $format->chain;
         $chain_model = $chain->model->name;
@@ -111,16 +125,18 @@ class Model
             $chain_field => $values
         ]);
 
-        if(!$chain_rows)
+        if (!$chain_rows) {
             return self::asArray($values);
+        }
 
         $parent_chains = [];
-        foreach($chain_rows as $row){
+        foreach ($chain_rows as $row) {
             $parent_id = $row->{$chain_field};
             $child_id  = $row->{$chain->identity};
 
-            if(!isset($parent_chains[$parent_id]))
+            if (!isset($parent_chains[$parent_id])) {
                 $parent_chains[$parent_id] = [];
+            }
             $parent_chains[$parent_id][] = $child_id;
         }
 
@@ -131,13 +147,15 @@ class Model
 
         $result = [];
 
-        foreach($parent_chains as $parent => $childs){
-            if(!isset($result[$parent]))
+        foreach ($parent_chains as $parent => $childs) {
+            if (!isset($result[$parent])) {
                 $result[$parent] = [];
+            }
 
-            foreach($childs as $child){
-                if(isset($children[$child]))
+            foreach ($childs as $child) {
+                if (isset($children[$child])) {
                     $result[$parent][] = $children[$child];
+                }
             }
         }
 
@@ -146,18 +164,20 @@ class Model
 
     static function children(array $values, string $field, array $objects, object $format, $options): array
     {
-        if(is_null($options))
+        if (is_null($options)) {
             return self::asArray($values);
+        }
 
         $children = self::procValues($values, $format, $options, 'id');
         $result   = [];
         $parent   = $format->model->field;
 
-        foreach($children as $child){
+        foreach ($children as $child) {
             $par_id = (int)(string)$child->$parent;
 
-            if(!isset($result[$par_id]))
+            if (!isset($result[$par_id])) {
                 $result[$par_id] = [];
+            }
 
             $result[$par_id][] = $child;
         }
@@ -259,13 +279,14 @@ class Model
         $cases      = $format->cases;
 
         $map_values = [];
-        foreach($objects as $object){
+        foreach ($objects as $object) {
             $case_value = $object->{$case_field};
             $obj_value  = $object->$field;
-            if(is_null($obj_value))
+            if (is_null($obj_value)) {
                 continue;
+            }
 
-            if(!isset($map_values[$case_value])){
+            if (!isset($map_values[$case_value])) {
                 $map_values[$case_value] = (object)[
                     'case'   => $case_value,
                     'values' => [],
@@ -275,28 +296,38 @@ class Model
             $map_values[$case_value]->values[] = $object->$field;
         }
 
-        foreach($map_values as $type => &$vals){
+        foreach ($map_values as $type => &$vals) {
             $vals->values = array_unique($vals->values);
 
-            if(!isset($cases->{$type})){
-                foreach($vals->values as $val)
-                    $vals->result[$val] = new Std($val);
+            if (!isset($cases->{$type})) {
+                $vals->result = self::asId($vals->values);
                 continue;
             }
+            $case = $cases->{$type};
 
-            $case   = $cases->{$type};
-            $vals->result = self::procValues($vals->values, $case, $options);
+            if (is_null($options)) {
+                $vals->result = self::asId($vals->values);
+                if(isset($case->model->type)){
+                    foreach($vals->result as $index => $val){
+                        $val->id = Formatter::typeApply($case->model->type, $val->id, 'id', $val, (object)[], null);
+                        $vals->result[$index] = $val;
+                    }
+                }
+            } else {
+                $vals->result = self::procValues($vals->values, $case, $options);
+            }
         }
         unset($vals);
 
         $result = [];
 
-        foreach($objects as $object){
+        foreach ($objects as $object) {
             $case_value = $object->{$case_field};
             $obj_value  = $object->$field;
             $obj_id     = $object->id;
-            if(is_null($obj_value))
+            if (is_null($obj_value)) {
                 continue;
+            }
 
             $case_result = $map_values[$case_value]->result;
             $result[$obj_id] = $case_result[$obj_value] ?? new Std($obj_value);
@@ -307,8 +338,9 @@ class Model
 
     static function partial(array $values, string $field, array $objects, object $format, $options): array
     {
-        if(is_null($options))
+        if (is_null($options)) {
             return self::asNull($values);
+        }
         return self::procValues($values, $format, $options);
     }
 }
